5473069312d5c31c1d100971d72de2ebf2dbc68f
Angel Mata <joseglopez85@gmail.com>
2023-08-22 00:18:26 -0400

    Añadiendo KV lang

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.button import Button
 
 class MyApp(App):
     def build(self):
-        return Button(text = "Evaluación")
+        pass
 
 if __name__ == "__main__":
     MyApp().run()
