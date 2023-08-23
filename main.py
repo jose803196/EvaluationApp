e803aa1c281bb658dc6cbc1b8d1ad9ec37c091df
Angel Mata <joseglopez85@gmail.com>
2023-08-23 17:25:46 -0400

    Add Screens

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
-class FirstScreen(BoxLayout):
+class CrearScreen(Screen):
+    pass
+
+class VerScreen(Screen):
+    pass
+
+class EditarScreen(Screen):
+    pass
+
+class PrinScreen(Screen):
     pass
 
 class MyApp(App):
-    pass
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(PrinScreen(name ='start'))
+        sm.add_widget(CrearScreen(name="crear"))
+        sm.add_widget(VerScreen(name="ver"))
+        sm.add_widget(EditarScreen(name="editar"))
+        return sm
 
 if __name__ == "__main__":
     MyApp().run()
