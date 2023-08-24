dedba9113397a49c87714f73deef7fa5225aa0c7
Angel Mata <joseglopez85@gmail.com>
2023-08-23 20:45:00 -0400

    Fix Name of database and add color button

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 import sqlite3
+from sqlite3 import OperationalError
 
 
 class CrearScreen(Screen):
     def crear_tabla(self):
-        conn = sqlite3.connect('Notas.db')
-        cursor = conn.cursor()
-        
-        cursor.execute("""CREATE TABLE Programacion 
-                    (ID integer primary key, name text, apellido text, cedula integer, correo text)""")
-        conn.commit()
-        conn.close()
+        try:
+            conn = sqlite3.connect('Notas.db')
+            cursor = conn.cursor()
+            cursor.execute("""CREATE TABLE Programacion 
+                        (ID integer primary key, name text, apellido text, cedula integer, correo text)""")
+            conn.commit()
+            conn.close()
+        except OperationalError as e:
+            pass
 
 class VerScreen(Screen):
     pass
