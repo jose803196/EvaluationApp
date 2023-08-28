35d24851c68fef42adf2b185fefc4f314d71b127
Angel Mata <joseglopez85@gmail.com>
2023-08-28 12:17:27 -0400

    Fix Buttons part1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
+from kivy.graphics import Rectangle, Color
 from kivy.uix.screenmanager import Screen, ScreenManager
 import sqlite3
 from sqlite3 import OperationalError
@@ -31,6 +34,7 @@ class PrinScreen(Screen):
 
 class MyApp(App):
     def build(self):
+        Window.clearcolor = (1,1,1,1)
         sm = ScreenManager()
         sm.add_widget(PrinScreen(name ='start'))
         sm.add_widget(CrearScreen(name="crear"))
